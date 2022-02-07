61a5ac33176a452648d9312a95f5d7ddbe9c4312
Ej1seven <erikhunter@erikhunter.dev>
2022-02-07 16:49:47 -0600

    adding files to project

---
+++ functions.js
@@ -0,0 +1,28 @@
+var add = function (a, b) {
+    return a + b;
+};
+var subtract = function (a, b) {
+    return a - b;
+};
+function divide(a, b) {
+    return a / b;
+}
+var multiply = function (a, b) {
+    return a * b;
+};
+var logger = function (message) {
+    console.log(message);
+};
+var throwError = function (message) {
+    throw new Error(message);
+};
+var todaysWeather = {
+    date: new Date(),
+    weather: 'sunny'
+};
+var logWeather = function (_a) {
+    var date = _a.date, weather = _a.weather;
+    console.log(date);
+    console.log(weather);
+};
+logWeather(todaysWeather);
